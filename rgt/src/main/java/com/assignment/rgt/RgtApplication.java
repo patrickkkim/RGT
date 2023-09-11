86ee8849a28a62fcecd254bda0f0a1db63988c39
patrickkkim <patkim97@gmail.com>
2023-09-11 16:30:52 +0900

    Initial commit

---
+++ rgt/src/main/java/com/assignment/rgt/RgtApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.rgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgtApplication.class, args);
+	}
+
+}
